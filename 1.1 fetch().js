20633882c317c4f666bbecd64ccb5401eae38b15
Mr Footwork <pandau.ting@gmail.com>
2020-07-07 21:00:35 +0200

    Fetch Mastery
    I turned the sequential solution into a parallel one.

--- 1.1 fetch().js
+++ 1.1 fetch().js
@@ -1,23 +1,49 @@
-// catchRainbow('rainbow', '../images/Shuttle-Icon.jpg').catch(error => {
-// 	console.log('Oh nein! Das Shuttlecock konnte nicht geladen werden!')
-// 	console.error(error)
-// })
-// catchRainbow('pandas', '../images/pandas.jpg').catch(error => {
-// 	console.log('Oh nein! Die Pandas konnten nicht geladen werden!')
-// 	console.error(error)
-// })
-
-const allTheImages = [
+const IMAGES = [
 	[ 'shuttle', '../images/Shuttle-Icon.jpg' ],
 	[ 'pandas', '../images/pandas.jpg' ],
 	[ 'regex', '../images/regex.jpg' ],
 	[ 'edabit', '../images/edabit.png' ],
 ]
 
-catchAllImages(allTheImages).catch(error => {
-	console.log('Irgendwas hat nicht geklappt...')
-	console.error(error)
-})
+/**||||||||||||||||||||||||||||||
+ * 	  Fetch an array of images (parallel)
+ * |||||||||||||||||||||||||||||
+ */
+async function catchImagesParallel(arrImages) {
+	for (let i = 0; i < arrImages.length; i++) {
+		const imageTag = document.createElement('img')
+		document.body.appendChild(imageTag)
+		imageTag.id = arrImages[i][0]
+	}
+	let responses = []
+	let blobs = []
+	await Promise.all(
+		arrImages.map(async (path, i) => {
+			responses[i] = await fetch(path[1])
+			return responses[i]
+		})
+	)
+	await Promise.all(
+		responses.map(async (response, i) => {
+			blobs[i] = await response.blob()
+			return blobs[i]
+		})
+	)
+	for (let i = 0; i < arrImages.length; i++) {
+		const imageTag = document.getElementById(arrImages[i][0])
+		imageTag.src = URL.createObjectURL(blobs[i])
+		imageTag.style.width = '150px'
+	}
+}
+
+/**||||||||||||||||||||||||||||||
+ * 	  Fetch an array of images (sequential)
+ * |||||||||||||||||||||||||||||
+ */
+// catchAllImages(IMAGES).catch(error => {
+// 	console.log('Irgendwas hat nicht geklappt...')
+// 	console.error(error)
+// })
 async function catchAllImages(arrImages) {
 	const response = []
 	const blob = []
@@ -31,8 +57,24 @@ async function catchAllImages(arrImages) {
 		imageTag.style.width = '300px'
 	}
 }
+catchImagesParallel(IMAGES).catch(error => {
+	console.log('There occurred an error! What the heck happened?')
+	console.error(error)
+})
+/**||||||||||||||||||||||||||||||
+ * 			Fetch a picture
+ * |||||||||||||||||||||||||||||
+ * const catchRainbow = async () => {} unterstützt KEIN hoisting!
+ */
+// catchRainbow('rainbow', '../images/Shuttle-Icon.jpg').catch(error => {
+// 	console.log('Oh nein! Das Shuttlecock konnte nicht geladen werden!')
+// 	console.error(error)
+// })
+// catchRainbow('pandas', '../images/pandas.jpg').catch(error => {
+// 	console.log('Oh nein! Die Pandas konnten nicht geladen werden!')
+// 	console.error(error)
+// })
 
-// const catchRainbow = async () => {} unterstützt KEIN hoisting!
 async function catchRainbow(imageID, imagePath) {
 	const image = document.createElement('img')
 	document.body.appendChild(image)
@@ -43,14 +85,11 @@ async function catchRainbow(imageID, imagePath) {
 	image.style.width = '300px'
 }
 
-/**
- * TODO fetch more than one image
- * TODO fetch an array of images
- * TODO file as text file and paste text to DOM 
- * 
- * 
+/**||||||||||||||||||||||||||||||
+ * 			Fetch a Text
+ * |||||||||||||||||||||||||||||
  */
-fetchText().catch(error => (console.log('ein Fehler!'), console.error(error)))
+// fetchText().catch(error => (console.log('ein Fehler!'), console.error(error)))
 async function fetchText() {
 	const domText = document.createElement('p')
 	document.body.appendChild(domText)
